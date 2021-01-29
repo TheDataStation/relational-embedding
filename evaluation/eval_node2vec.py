c8d52e4d2aecfabe114fa07f9ac7d05e48b6388a
Alex Zhao <zhaozixuan0729@126.com>
2021-01-29 20:45:04 +0000

    add more evaluation modules

--- evaluation/eval_node2vec.py
+++ evaluation/eval_node2vec.py
@@ -28,12 +28,21 @@ with open("../embedding_config.json", "r") as jsonfile:
 num_bins = embeddding_config["num_bins"]
 test_size = embeddding_config["test_size"]
 
-
-def classification_task(X_train, X_test, y_train, y_test, n_estimators=100):
+def classification_task_rf(X_train, X_test, y_train, y_test, n_estimators=100):
     rf = RandomForestClassifier(n_estimators=n_estimators)
-    logr = LogisticRegression(penalty='l2', solver='liblinear')
-
     rf.fit(X_train, y_train)
+
+    def show_stats(model):
+        pscore_train = accuracy_score(y_train, model.predict(X_train))
+        pscore_test = accuracy_score(y_test, model.predict(X_test))
+        print("Train accuracy {}, Test accuracy {}".format(
+            pscore_train, pscore_test))
+        return pscore_train, pscore_test
+
+    return show_stats(rf)
+    
+def classification_task_logr(X_train, X_test, y_train, y_test, n_estimators=100):
+    logr = LogisticRegression(penalty='l2', solver='liblinear')
     logr.fit(X_train, y_train)
 
     def show_stats(model):
@@ -41,9 +50,9 @@ def classification_task(X_train, X_test, y_train, y_test, n_estimators=100):
         pscore_test = accuracy_score(y_test, model.predict(X_test))
         print("Train accuracy {}, Test accuracy {}".format(
             pscore_train, pscore_test))
+        return pscore_train, pscore_test
 
-    show_stats(rf)
-    show_stats(logr)
+    return show_stats(logr)
 
 
 def classification_task_nn(task, X_train, X_test, y_train, y_test):
@@ -84,12 +93,10 @@ def evaluate_task(args):
         "../", location_processed), sep=',', encoding='latin')
     full_table = pd.read_csv(os.path.join(
         "../", location + target_file), sep=',', encoding='latin')
-    Y = full_table[target_column]
 
+    Y = full_table[target_column]
     if args.task in ["kraken", "financial", "genes"]:
         Y = pd.Categorical(Y).codes
-    if args.task == "sample":
-        Y = Y.apply(lambda x: int(x / 200))
 
     # Set embeddings that are to be evaluated
     method = args.method
@@ -101,24 +108,43 @@ def evaluate_task(args):
         model = KeyedVectors.load_word2vec_format(path)
         table_name = path.split("/")[-1][:-4]
         model_dict_path = "../graph/{}/{}.dict".format(args.task, table_name)
+        print(model_dict_path)
 
         # Obtain textified & quantized data
-        df_textified = EU.textify_df(
-            trimmed_table, strategies, location_processed)
-        x_vec = pd.DataFrame(EU.vectorize_df(
-            df_textified, model, model_dict=model_dict_path, model_type=method))
-        x_vec = x_vec.dropna(axis=1)
-
-        # Train a Random Forest classifier
-        # remove_hubness_and_run(x_vec, Y)
-        X_train, X_test, y_train, y_test = train_test_split(
-            x_vec, Y, test_size=0.1, random_state=10)
-        print("Evaluating model", path)
-        # EU.remove_hubness_and_run(x_vec, Y)
-
-        for n_estimators in [10, 50, 100]:
-            classification_task(X_train, X_test, y_train,
-                                y_test, n_estimators=n_estimators)
+        training_loss = [] 
+        testing_loss = [] 
+        for i in range(0, 100, 10):
+            df_textified = EU.textify_df(
+                trimmed_table, strategies, location_processed)
+            x_vec = pd.DataFrame(
+                EU.vectorize_df(
+                    df_textified, model, model.vocab,
+                    model_dict=model_dict_path, model_type=method
+                )
+            )
+            model_2dim = EU.get_PCA_for_embedding(model)
+            x_vec_2dim = pd.DataFrame(
+                EU.vectorize_df(
+                    df_textified, model_2dim, model.vocab,
+                    model_dict=model_dict_path, model_type=method
+                )
+            )
+            
+            # Train a Random Forest classifier
+            tests = train_test_split(x_vec, Y, test_size=0.1, random_state=10)
+            train_loss, test_loss = classification_task_logr(*tests)
+            training_loss.append(train_loss)
+            testing_loss.append(test_loss)
+        print(training_loss)
+        print(testing_loss)
+        # tests_2dim = train_test_split(x_vec_2dim, Y, test_size=test_size, random_state=10)
+        # print("{}_dim:".format(model.vector_size))
+        # for n_estimators in [10, 50, 100]:
+        #     classification_task(*tests, n_estimators=n_estimators)
+        # print("2_dim:")
+        # for n_estimators in [10, 50, 100]:
+        #     classification_task(*tests_2dim, n_estimators=n_estimators)
+            
         # classification_task_nn(args.task, X_train, X_test, y_train, y_test)
 
 
--- evaluation/eval_utils.py
+++ evaluation/eval_utils.py
@@ -14,20 +14,31 @@ with open("../embedding_config.json", "r") as jsonfile:
     embeddding_config = json.load(jsonfile)
 num_bins = embeddding_config["num_bins"]
 
+
 def all_files_in_path(path, task):
-    fs = [join(path, f) for f in listdir(path) if isfile(join(path, f)) and f != ".DS_Store" and f.find(task) != -1]
+    fs = [join(path, f) for f in listdir(path)
+          if isfile(join(path, f)) and f != ".DS_Store"
+          and f.find(task) != -1 and f[-4:] == ".emb"]
     return fs
 
-def vectorize_df(df, model, model_dict = None, model_type = "word2vec"):
+
+def get_PCA_for_embedding(model, ndim=2):
+    from sklearn.decomposition import PCA
+    words = list(model.vocab.keys())
+    word_vectors = np.array([model[w] for w in words])
+
+    twodim = pd.DataFrame(
+        PCA().fit_transform(word_vectors)[:, :ndim],
+        columns=["d" + str(i+1) for i in range(ndim)]
+    )
+    twodim.index = words
+    return twodim.T
+
+
+def vectorize_df(df, model, model_vocab=None, model_dict=None, model_type="word2vec"):
     length = len(df)
     x_vectorized = [[] for i in range(length)]
-    # if model_type == "word2vec":
-    #     for i in range(length):
-    #         row = df[i]
-    #         for j in range(len(row)):
-    #             if row[j] in model.vocab:
-    #                 x_vectorized[i] += list(model[row[j]])
-    
+
     if model_type == "node2vec" or model_type == "ProNE":
         from token_dict import TokenDict
         cc = TokenDict()
@@ -35,11 +46,10 @@ def vectorize_df(df, model, model_dict = None, model_type = "word2vec"):
         for i in range(length):
             row = df[i]
             for j in range(len(row)):
-                if cc.check(row[j]) in model.vocab:
+                if cc.check(row[j]) in model_vocab:
                     x_vectorized[i] += list(model[cc.check(row[j])])
     return x_vectorized
 
-# Assume that textification strategy is skip (integer), row & col, sequence text
 def textify_df(df, strategies, path):
     table_name = path.split("/")[-1]
     df = tr.quantize(df, strategies[table_name])
@@ -52,19 +62,21 @@ def textify_df(df, strategies, path):
         decoded_value = dpu.encode_cell(cell_value, grain=grain_strategy)
         for value in decoded_value:
             input[row].append(value)
-    
+
     filename = "".join(table_name.split(".")[:-1])
     for row in range(df.shape[0]):
         row_name = "{}_row:{}".format(filename, str(row))
         input[row].append(row_name)
     return input
 
+
 def measure_quality(ground_truth, predicted_truth):
-    precision = [] 
+    precision = []
     for i in range(len(ground_truth)):
         flag = ground_truth[i] in predicted_truth[i]
         precision.append(flag)
     return precision
+
 
 def remove_hubness_and_run(X, y, n_neighbors=15):
     from skhubness import Hubness
@@ -76,30 +88,32 @@ def remove_hubness_and_run(X, y, n_neighbors=15):
     hub.fit(X)
     k_skew = hub.score()
     print(f'Skewness = {k_skew:.3f}')
-    
+
     hub_mp = Hubness(k=10, metric='cosine',
-                 hubness='mutual_proximity')
+                     hubness='mutual_proximity')
     hub_mp.fit(X)
     k_skew_mp = hub_mp.score()
     print(f'Skewness after MP: {k_skew_mp:.3f} '
-        f'(reduction of {k_skew - k_skew_mp:.3f})')
+          f'(reduction of {k_skew - k_skew_mp:.3f})')
     print(f'Robin hood: {hub_mp.robinhood_index:.3f} '
-        f'(reduction of {hub.robinhood_index - hub_mp.robinhood_index:.3f})')
+          f'(reduction of {hub.robinhood_index - hub_mp.robinhood_index:.3f})')
 
-    # Measure Classfication Accuracy before and after removal 
+    # Measure Classfication Accuracy before and after removal
     # vanilla kNN
-    knn_standard = KNeighborsClassifier(n_neighbors=n_neighbors, metric='cosine')
+    knn_standard = KNeighborsClassifier(
+        n_neighbors=n_neighbors, metric='cosine')
     acc_standard = cross_val_score(knn_standard, X, y, cv=10)
 
     # kNN with hubness reduction (mutual proximity)
     knn_mp = KNeighborsClassifier(n_neighbors=n_neighbors,
-                              metric='cosine',
-                              hubness='mutual_proximity')
+                                  metric='cosine',
+                                  hubness='mutual_proximity')
     acc_mp = cross_val_score(knn_mp, X, y, cv=10)
 
     print(f'Accuracy (vanilla kNN): {acc_standard.mean():.3f}')
     print(f'Accuracy (kNN with hubness reduction): {acc_mp.mean():.3f}')
     return (acc_standard.mean(), acc_mp.mean())
+
 
 def parse_strategy(s):
     integer_strategy = "quantize"
@@ -113,7 +127,7 @@ def parse_strategy(s):
         textification_strategy = "row"
     elif s.find("col") != -1:
         textification_strategy = "col"
-    
+
     if s.find("quantize") != -1:
         integer_strategy = "quantize"
     elif s.find("stringify") != -1:
